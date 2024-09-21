6724c1267b3d89030833f26fc497a46acda1ee7a
unknown <giorgilagidze25>
2024-09-21 16:39:35 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+    const firstName = 'Giorgi'
+
+  return (
+     <div>
+      <h1 className='title'>hello {firstName}</h1>
+     </div>
+  )
+}
+
+export default App
